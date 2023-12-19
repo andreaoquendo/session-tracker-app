dc886f960cb0383416c8c6b8ba1f6496bb68ccee
Andrea Oquendo <oquendo.3455@gmail.com>
2023-12-19 20:52:32 -0300

    Initial Commit

---
+++ SessionTracker/SessionTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SessionTrackerApp.swift
+//  SessionTracker
+//
+//  Created by Andrea Oquendo on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SessionTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
