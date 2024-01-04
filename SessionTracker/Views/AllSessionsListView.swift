260320d76be016b9c8e858da1ab1150ff7bb71dd
Andrea Oquendo <oquendo.3455@gmail.com>
2024-01-04 00:36:03 -0300

    feat:  create recent sessions view files

---
+++ SessionTracker/Views/AllSessionsListView.swift
@@ -0,0 +1,18 @@
+//
+//  AllSessionsListView.swift
+//  SessionTracker
+//
+//  Created by Andrea Oquendo on 03/01/24.
+//
+
+import SwiftUI
+
+struct AllSessionsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllSessionsListView()
+}
